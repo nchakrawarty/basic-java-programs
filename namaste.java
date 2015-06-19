9a41901172f03aaece043754eb0429e7a90cd1cd
nchakrawarty <nchakrawarty@gmail.com>
2015-06-20 01:45:48 +0530

    Revert "Revert "Revert "Revert "java programs""""
    This reverts commit bd17cbe697a6b971ed8b431a5d2562c7808d2cf7.

---
+++ namaste.java
@@ -0,0 +1,47 @@
+class namaste {
+
+}
+
+class add{
+	public static void main(String[] asd)
+	{
+	int a=10;
+	int b=20;
+	int c;
+	c=a+b;
+	System.out.println(" Sum off " +a+ " and " +b+ " is =");
+	System.out.println(+c);
+	}
+	
+}
+
+class hello {
+	public static void main(String[] felight)
+	{
+	System.out.println("Hello felight");
+	
+	}
+	
+}
+
+class area {
+
+public static void main(String[] asd)
+	{
+	int rad=20;
+	double area;
+	area=rad*rad*3.142;
+	System.out.println("area of circle =" +area);
+	}
+}
+
+class speed {
+	public static void main(String[] felight) {
+	int time=120;
+	int distance=600;
+	int speed= distance/time;
+	System.out.printf("Speed is = %d if time is %d and distance is %d",speed,time,distance);
+	}
+	
+}
+	
